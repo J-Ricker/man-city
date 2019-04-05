2ba36c63299b745cabc236c56394fb9107c3dbd4
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-05 11:09:41 -0400

    setup routes.js, imported new components

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './HOC/layout';
+
+
+const Routes = (props) => {
+  return (
+    <Layout>
+      Second Line
+      
+    </Layout>
+  )
+}
+export default Routes;
