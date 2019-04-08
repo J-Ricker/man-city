5621101e093ab3f59f362c6dbf7ccc8f02c4d1ca
Jeremy R <lvlgrinddesign@gmail.com>
2019-04-08 08:57:54 -0400

    imported firebase, restrustured imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
 import './resources/css/app.css';
 import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
+import './firebase';
+
 
 const App = () => {
     return (
