a0f083958fda972a9d5bec59f85a796e03c63bfa
Jeremy R <lvlgrinddesign@gmail.com>
2019-04-08 08:57:15 -0400

    created database file, connected to firebase db

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+
+
+const config = {
+    apiKey: "AIzaSyA9D9NSqBUwyvSArRVGOfzEMoNoaIqcfT0",
+    authDomain: "man-city-76299.firebaseapp.com",
+    databaseURL: "https://man-city-76299.firebaseio.com",
+    projectId: "man-city-76299",
+    storageBucket: "man-city-76299.appspot.com",
+    messagingSenderId: "666915138661"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.database();
+
+db.ref('matches').once('value').then((snapshot) => {
+    console.log(snapshot.val());
+})
