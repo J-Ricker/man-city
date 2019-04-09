8e8da52f9d0dcc76da05da0e53c1c1e3469399bc
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-09 18:29:12 -0400

    created index.js for promotion section

---
+++ src/components/home/promotion/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Promo from './promo';
+
+const Promotion = () => {
+    return (
+        <div className="promotion_wrapper" style={{
+            background: '#ffffff'
+        }}>
+            <div className="container">
+                <Promo />
+            </div>
+        </div>
+    )
+}
+
+export default Promotion;
