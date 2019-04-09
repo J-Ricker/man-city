6f6913d2bdd25b60bc83237138f41dcfc5e5ac84
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-09 18:40:48 -0400

    added react component to enroll

---
+++ src/components/home/promotion/enroll.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Enroll extends Component {
+
+    state = {
+        formError: false,
+        formSuccess: '',
+        formdata: {
+            email: {
+                
+            }
+        }
+    }
+
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
+
+export default Enroll;
