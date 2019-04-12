92e42fa37a86ae3eb3cdda6180b48317c2faea02
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-12 11:41:20 -0400

    added additional object value fields and values

--- src/components/home/promotion/enroll.js
+++ src/components/home/promotion/enroll.js
@@ -1,22 +1,74 @@
 import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade';
+import FormField from '../../ui/formFields';
 
 class Enroll extends Component {
 
     state = {
         formError: false,
         formSuccess: '',
-        formdata: {
+        formData: {
             email: {
-                
+                element: 'input',
+                value: '',
+                config: {
+                    name:'email_input',
+                    type: 'email',
+                    placeholder: 'enter your email'
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: {
+                    valid: false,
+                    validationMessage: ''
+                }
             }
         }
     }
 
+    updateForm(element) {
+        const newFormData = {
+            ...this.state.formData
+        }
+        const NewElement = {
+            ...newFormData[element.id]
+        }
+
+        NewElement.value = element.event.target.value;
+
+        newFormData[element.id] = NewElement;
+
+        this.setState({
+            formData: newFormData
+        })
+    }
+
+    submitForm() {
+
+    }
+
     render() {
         return (
-        <div>
-            
-        </div>
+            <Fade>
+                <div className="enroll_wrapper">
+                    <form onSubmit={(e) => this.submitForm(e)} >
+                        <div className="enroll_title">
+                            Enter your email
+                        </div>
+                        <div className="enroll_input">
+                            <FormField 
+                                id={'email'}
+                                formData={this.state.formData.email}
+                                change ={(element) => this.updateForm(element)}
+
+                            />
+                        </div>
+                    </form>
+                </div>
+
+            </Fade>
         )
     }
 }
