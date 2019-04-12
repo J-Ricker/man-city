c8b681bdb5ee7f92fec7062b6fb60394940c3e68
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-12 13:42:09 -0400

    set for valid and user registration to firebase

--- src/components/home/promotion/enroll.js
+++ src/components/home/promotion/enroll.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Fade from 'react-reveal/Fade';
 import FormField from '../../ui/formFields';
 import { validate } from '../../ui/misc';
-
+import { firebasePromo } from '../../../firebase';
 
 class Enroll extends Component {
 
@@ -47,15 +47,69 @@ class Enroll extends Component {
 
         newFormData[element.id] = newElement;
 
-        console.log(newFormData);
 
         this.setState({
+            formError: false,
             formData: newFormData
         })
     }
 
-    submitForm() {
+    resetFormSuccess(type) {
+        const newFormData = {
+            ...this.state.formData
+        }
 
+        for(let key in newFormData) {
+            newFormData[key].value = '';
+            newFormData[key].valid = '';
+            newFormData[key].validationMessage = '';
+        }
+
+        this.setState({
+            formError: false,
+            formData: newFormData,
+            formSuccess: type ? 'Congratulations' : 'Already in database'
+        });
+
+        this.clearSuccess();
+    }
+
+    clearSuccess() {
+        setTimeout(() => {
+            this.setState({
+                formSuccess: ''
+            })
+        }, 2000) 
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+
+        let dataToSubmit = {
+
+        }
+        let formisValid = true;
+        
+        for (const key in this.state.formData) {
+            dataToSubmit[key] = this.state.formData[key].value;
+            formisValid = this.state.formData[key].valid && formisValid;
+        }
+
+        if(formisValid) {
+            firebasePromo.orderByChild('email').equalTo(dataToSubmit.email).once('value')
+            .then((snapshot) => {
+                if(snapshot.val() === null) {
+                    firebasePromo.push(dataToSubmit);
+                    this.resetFormSuccess(true)
+                } else {
+                    this.resetFormSuccess(false)
+                }
+            })
+        } else {
+            this.setState({
+                formError: true
+            })
+        }
     }
 
     render() {
@@ -73,10 +127,18 @@ class Enroll extends Component {
                                 change={(element) => this.updateForm(element)}
 
                             />
+
+                            {this.state.formError ? <div className="error_label">
+                                Something is wrong here, try again.
+                            </div> 
+                            : null}
+                            <div className="success_label">
+                                {this.state.formSuccess}
+                            </div>
+                            <button onClick={(e) => this.submitForm(e)}>Enroll</button>
                         </div>
                     </form>
                 </div>
-
             </Fade>
         )
     }
