a6c0481ce89af4d546d09e44c6b237feac6547ae
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-18 10:52:09 -0400

    imported new signin section to routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import Layout from './HOC/layout';
 import {Switch, Route} from 'react-router-dom';
 
 import Home from './components/home';
+import SignIn from './components/signIn';
 
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={SignIn} path="/sign-in" />
         <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
