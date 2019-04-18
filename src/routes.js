40b6018904d7bf4e8a208a2456b22690b4a7733d
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-18 11:44:37 -0400

    imported admin component

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './components/home';
 import SignIn from './components/signIn';
 
+import Dashboard from './components/admin/dashboard';
+
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
         <Route exact component={SignIn} path="/sign-in" />
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
